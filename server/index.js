eaba52899d74dead62715cdd2c9a575ddc60363a
shourov <shourovys@gmail.com>
2020-11-25 09:38:49 +0600

    .env file added

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const postRouter = require('./Routers/PostRouters');
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 // eslint-disable-next-line prettier/prettier
-const url = 'mongodb+srv://readRight:readRight@cluster0.vb8a7.mongodb.net/datastore?retryWrites=true&w=majority';
+const url = process.env.URL;
 
 mongoose
     .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
