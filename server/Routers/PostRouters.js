47a042e59fe53c358a0617c579fa93a9fce50ed2
shourov <shourovys@gmail.com>
2020-12-03 20:08:12 +0600

    back-end done

--- server/Routers/PostRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPostData, createPost } = require('../Controllers/PostControllers');
-
-const postRouter = express.Router();
-
-postRouter.post('/', createPost);
-postRouter.get('/', getPostData);
-
-module.exports = postRouter;
--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const postRouter = require('./Routers/PostRouters');
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+
 
 const app = express();
-dotenv.config();
-const port = process.env.PORT || 4000;
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-// eslint-disable-next-line prettier/prettier
-const url = process.env.URL;
+app.use('/posts', postRoutes);
 
-mongoose
-    .connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => app.listen(port, () => console.log(`your app is listen on ${port}`)))
-    .catch((err) => console.log(err));
+app.get('/',(req,res)=>res.send('welcome to social baddy'))
 
-app.use('/posts', postRouter);
+const CONNECTION_URL = 'mongodb+srv://readRight:readRight@cluster0.vb8a7.mongodb.net/newsocialbaddy?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
